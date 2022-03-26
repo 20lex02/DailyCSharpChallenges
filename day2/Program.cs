399bdd15711efa5bdb744443ba6892921a42606d
Alexandre Laroche <LARA69060208@collegealma.ca>
2022-03-26 13:05:07 -0400

    Did day 2

---
+++ day2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+public static class Program 
+{
+    public static void Main(string[] args) 
+    {
+        WriteArray(ProductIgnore(new[] { 1, 2, 3, 4, 5 })); // [120,60,40,30,24]
+        WriteArray(ProductIgnore(new[] { 3, 2, 1 })); // [2, 3, 6]
+    }
+
+    public static int[] ProductIgnore(int[] values) 
+    {
+        // Fill the return array with 1s
+        var returnValues = Enumerable.Repeat(1, values.Length).ToArray();
+
+        for (int i = 0; i < values.Length; i++) {
+            // Foreach element in the array
+            for (int j = 0; j < values.Length; j++) {
+                // Foreach element in the array
+                if (i == j) {
+                    // If they are at the same index, skip it...
+                    continue;
+                }
+
+                // Else multiply the value of the return values to the passed value
+                returnValues[i] *= values[j];
+            }
+        }
+
+        return returnValues;
+    }
+
+    public static void WriteArray(IEnumerable<int> val) 
+    {
+        Console.WriteLine("[" + string.Join(",", val) + "]");
+    }
+}
---
+++ day2/readme.md
@@ -0,0 +1,8 @@
+## Day 2: ProductIgnore
+Given an array of integer, return a new array such that each element et index `i` of the new array is the product of all the numbers in the original array except from the one at `i`.
+### Examples
+* `ProductIgnore(new[] { 1, 2, 3, 4, 5 })` -> `[120,60,40,30,24]`
+* `ProductIgnore(new[] { 3, 2, 1 })` -> `[2, 3, 6]`
+#
+Done [X]  
+Bonus: Can you do this without division [X]
