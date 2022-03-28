b20bc82ad0996cc3d998819422dbb61f9638d7eb
al.laroche <al.laroche@cgi.com>
2022-03-28 15:52:46 -0400

    Did day4

---
+++ day4/Program.cs
@@ -0,0 +1,15 @@
+﻿public static class Program 
+{
+    public static void Main(string[] args) {
+        Console.WriteLine(SmallestMissingPositive(new[] { 3, 4, -1, 1 }));
+        Console.WriteLine(SmallestMissingPositive(new[] { 1, 2, 0 }));
+    }
+
+    public static int SmallestMissingPositive(int[] values) {
+        var postiveValues = values.Where(num => num > 0);
+        var min = 1;
+        var max = postiveValues.Max() + 1;
+
+        return Enumerable.Range(min, max).Where(num => !postiveValues.Contains(num)).Min();
+    }
+}
---
+++ day4/readme.md
@@ -0,0 +1,7 @@
+## Smallest missing positive (hard)
+Given an array of integer, find the first missing positive integer. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicate and negative numbers as well.
+### Examples
+`SmallestMissingPositive(new[] { 3, 4, -1, 1 })` -> `2`  
+`SmallestMissingPositive(new[] { 1, 2, 0 })` -> `3`
+#
+Done [X]
