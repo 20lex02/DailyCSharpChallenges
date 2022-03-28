06bade405372b065e5ddf20bf6043438bdd8b787
al.laroche <al.laroche@cgi.com>
2022-03-28 15:09:33 -0400

    Did day3-2

---
+++ day3-2/Program.cs
@@ -0,0 +1,191 @@
+﻿using System.Linq;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var sudokuGridValid = new int?[9, 9]
+        {
+            {6,3,9,  5,7,4,  1,8,2},
+            {5,4,1,  8,2,9,  3,7,6},
+            {7,8,2,  6,1,3,  9,5,4},
+
+            {1,9,8,  4,6,7,  5,2,3},
+            {3,6,5,  9,8,2,  4,1,7},
+            {4,2,7,  1,3,5,  8,6,9},
+
+            {9,5,6,  7,4,8,  2,3,1},
+            {8,1,3,  2,9,6,  7,4,5},
+            {2,7,4,  3,5,1,  6,9,8}
+        };
+
+        var sudokuGridInvalid1 = new int?[9,9] 
+        {
+            {6,3,9,  5,7,4,  1,8,2},
+            {5,4,1,  8,5,9,  3,7,6},
+            {7,8,2,  6,1,3,  9,5,4},
+
+            {1,9,8,  4,6,7,  5,2,3},
+            {3,6,5,  9,8,2,  4,3,7},
+            {4,2,7,  1,3,5,  8,6,9},
+
+            {9,5,6,  7,4,8,  2,3,1},
+            {8,1,3,  2,2,6,  7,4,5},
+            {2,7,4,  3,5,1,  6,9,8}
+        };
+
+        int? o = null;
+        var sudokuGridInvalid2 = new int?[9,9] 
+        {
+            {6,3,9,  5,7,4,  1,8,2},
+            {5,4,1,  8,2,9,  3,7,6},
+            {7,8,2,  6,1,3,  9,o,4},
+
+            {1,9,8,  4,6,7,  5,2,3},
+            {3,6,5,  9,8,2,  4,1,7},
+            {4,2,7,  1,3,5,  8,6,9},
+
+            {9,5,6,  7,4,8,  2,3,1},
+            {8,1,3,  2,o,6,  7,4,5},
+            {2,7,4,  3,5,1,  6,9,8}
+        };
+
+        var sudokuGridInvalid3 = new int?[9,9] 
+        {
+            {6,3,9,  5,7,4,  1,8,2},
+            {5,4,1,  8,2,9,  3,7,6},
+            {7,8,2,  6,1,3,  9,0,4},
+        
+            {1,9,8,  4,6,7,  5,2,3},
+            {3,6,5,  9,8,2,  4,1,7},
+            {4,2,7,  1,3,5,  8,6,9},
+        
+            {9,5,6,  7,4,8,  2,3,1},
+            {8,10,3, 2,9,6,  7,4,5},
+            {2,7,4,  3,5,1,  6,9,8}
+        };
+
+        Console.WriteLine(CheckSudoku(sudokuGridValid));
+        Console.WriteLine(CheckSudoku(sudokuGridInvalid2));
+        Console.WriteLine(CheckSudoku(sudokuGridInvalid3));
+    }
+
+    public static bool CheckSudoku(int?[,] sudokuGrid) {
+        if ( !sudokuGrid.Cast<int?>().All(number => number is not null && ((int)number!).InRange(1,9)) ) {
+            // If not all numbers are valid, return false
+            return false;
+        }
+
+        // Separate the grid via lines, columns and quadrants
+        var lines = sudokuGrid.GetRows();
+        var columns = sudokuGrid.GetColumns();
+        var quadrants = sudokuGrid.GetQuadrants(3);
+
+        // Put them all together
+        IEnumerable<IEnumerable<int?>> components = lines.AppendAllEnumerables(columns, quadrants);
+
+        // Return true if they are all unique
+        return components.All(numbers => numbers.Unique());
+    }
+
+    /// <summary>
+    /// Checks wether or not an int is within the specified range (inclusive)
+    /// </summary>
+    /// <param name="min">The minimum value that the number can be (inclusive)</param>
+    /// <param name="max">The maximum value that the number can be (inclusive)</param>
+    /// <returns>
+    /// If the number is within the specified range
+    /// </returns>
+    public static bool InRange(this int val, int min, int max) {
+        return min <= val && val <= max;
+    }
+
+    /// <summary>
+    /// Gets the rows from a 2d array
+    /// </summary>
+    /// <returns>
+    /// The rows from a 2d array
+    /// </returns>
+    public static IEnumerable<IEnumerable<T>> GetRows<T>(this T[,] grid) {
+        for (int rowIndex = 0; rowIndex < grid.GetLength(0); rowIndex++){
+            var value = Enumerable.Range(0, grid.GetLength(1) - 1).Select(i => grid[rowIndex, i]);
+            yield return value;
+        }
+    }
+
+    /// <summary>
+    /// Gets the colums from a 2d array
+    /// </summary>
+    /// <returns>
+    /// The columns from a 2d array
+    /// </returns>
+    public static IEnumerable<IEnumerable<T>> GetColumns<T>(this T[,] grid) {
+        for (int colIndex = 0; colIndex < grid.GetLength(1); colIndex++) {
+            var value = Enumerable.Range(0, grid.GetLength(0) - 1).Select(i => grid[i, colIndex]);
+            yield return value;
+
+        }
+    }
+
+    /// <summary>
+    /// splits up a 2d array into quadrants of specified size and flattens them.
+    /// </summary>
+    /// <param name="quadrantsSize">The size of the quadrants</param>
+    public static IEnumerable<IEnumerable<T>> GetQuadrants<T>(this T[,] grid, int quadrantsSize) {
+        // Get the amount of numbers within one quadrant
+        var quadrantLength = quadrantsSize * quadrantsSize;
+        // Get the number of quadrants required to fill out the grid
+        var quadrantCountX = grid.GetLength(0) / quadrantsSize;
+        var quadrantCountY = grid.GetLength(1) / quadrantsSize;
+
+        for (int quadrantIndex = 0; quadrantIndex * quadrantIndex < grid.Length ; quadrantIndex++) {
+            // Get the global coordinates for the first number of the quadrant (top-left)
+            var globalRowIndex = (quadrantIndex / quadrantCountY) * quadrantCountX;
+            var globalColIndex = (quadrantIndex % quadrantCountY) * quadrantCountY;
+
+            // Select all the numbers within the quadrant                              
+            yield return Enumerable.Range(0, quadrantLength)
+                                           // Add the local position to the global one and return the value at the corresponding coordinates
+                .Select(localIndex => grid[globalRowIndex + localIndex / quadrantsSize, globalColIndex + localIndex % quadrantsSize]);
+        }
+    }
+
+    /// <summary>
+    /// Appends all the specified IEnumerables to a data source
+    /// </summary>
+    /// <param name="enumerablesToAppend">The IEnumerables to append to the data source</param>
+    /// <returns>
+    /// The appended original source
+    /// </returns>
+    public static IEnumerable<T> AppendAllEnumerables<T>(this IEnumerable<T> original, params IEnumerable<T>[] enumerablesToAppend) {
+        var returnValue = original;
+        foreach (var enumerable in enumerablesToAppend) {
+            returnValue.AppendEnumerable(enumerable);
+        }
+        
+        return returnValue;
+    }
+
+    /// <summary>
+    /// Appends the specified IEnumerable to a data source
+    /// </summary>
+    /// <param name="enumerable">The IEnumerable to append to the data source</param>
+    /// <returns>
+    /// The appended original source
+    /// </returns>
+    public static IEnumerable<T> AppendEnumerable<T>(this IEnumerable<T> original, IEnumerable<T> enumerable) {
+        var returnValue = original;
+        foreach (var value in enumerable) {
+            returnValue = returnValue.Append(value);
+        }
+        
+        return returnValue;
+    }
+
+    /// <summary>
+    /// Checks wether or not all elements are unique
+    /// </summary>
+    public static bool Unique<T>(this IEnumerable<T> source)
+    {
+        return source.GroupBy(item => item).Count() == source.Count();
+    }
+}
---
+++ day3-2/readme.md
@@ -0,0 +1,75 @@
+## Day 3-2: Sudoku checker
+Sudoku is a game in which you have to fill out a 9 x 9 grid of numbers in such a way that every column, row and 3 x 3 squares contains the number 1 through 9 only once.  
+\
+Create a function that takes in parameter a 2d nullable integer array (`int?[,]`) and checks wether or not it is valid. In order to be valid, all integers must not be null, and all rows, columns and quadrons (3x3 squares) must have all numbers from 1 to 9, without repetition.
+## Examples
+Given the following 2d array:
+```csharp
+var sudokuGridValid = new int?[9,9] 
+{
+    {6,3,9,  5,7,4,  1,8,2},
+    {5,4,1,  8,2,9,  3,7,6},
+    {7,8,2,  6,1,3,  9,5,4},
+
+    {1,9,8,  4,6,7,  5,2,3},
+    {3,6,5,  9,8,2,  4,1,7},
+    {4,2,7,  1,3,5,  8,6,9},
+
+    {9,5,6,  7,4,8,  2,3,1},
+    {8,1,3,  2,9,6,  7,4,5},
+    {2,7,4,  3,5,1,  6,9,8}
+};
+```
+The function should return `true`.  
+However, if given one of the following arrays:
+```csharp
+var sudokuGridInvalid1 = new int?[9,9] 
+{
+    {6,3,9,  5,7,4,  1,8,2},
+    {5,4,1,  8,5,9,  3,7,6},
+    {7,8,2,  6,1,3,  9,5,4},
+
+    {1,9,8,  4,6,7,  5,2,3},
+    {3,6,5,  9,8,2,  4,3,7},
+    {4,2,7,  1,3,5,  8,6,9},
+
+    {9,5,6,  7,4,8,  2,3,1},
+    {8,1,3,  2,2,6,  7,4,5},
+    {2,7,4,  3,5,1,  6,9,8}
+};
+int? o = null;
+var sudokuGridInvali2 = new int?[9,9] 
+{
+    {6,3,9,  5,7,4,  1,8,2},
+    {5,4,1,  8,2,9,  3,7,6},
+    {7,8,2,  6,1,3,  9,o,4},
+
+    {1,9,8,  4,6,7,  5,2,3},
+    {3,6,5,  9,8,2,  4,1,7},
+    {4,2,7,  1,3,5,  8,6,9},
+
+    {9,5,6,  7,4,8,  2,3,1},
+    {8,1,3,  2,o,6,  7,4,5},
+    {2,7,4,  3,5,1,  6,9,8}
+};
+
+var sudokuGridInvalid3 = new int?[9,9] 
+{
+    {6,3,9,  5,7,4,  1,8,2},
+    {5,4,1,  8,2,9,  3,7,6},
+    {7,8,2,  6,1,3,  9,0,4},
+
+    {1,9,8,  4,6,7,  5,2,3},
+    {3,6,5,  9,8,2,  4,1,7},
+    {4,2,7,  1,3,5,  8,6,9},
+
+    {9,5,6,  7,4,8,  2,3,1},
+    {8,10,3,  2,9,6,  7,4,5},
+    {2,7,4,  3,5,1,  6,9,8}
+};
+```
+The function should return `false`.
+### Notes
+* You can assume the passed array will always be 9*9
+#
+Done [ ]
