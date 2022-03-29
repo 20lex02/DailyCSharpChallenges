203eb9dc4cb9bbd57f3f24e626d3d5bc2c1cb8cd
al.laroche <al.laroche@cgi.com>
2022-03-29 08:36:19 -0400

    Did day4-1

---
+++ day4-1/Program.cs
@@ -0,0 +1,37 @@
+﻿public static class Program 
+{
+    public static void Main(string[] args) {
+        Console.WriteLine(Simplify("4/6"));
+        Console.WriteLine(Simplify("10/11"));
+        Console.WriteLine(Simplify("100/400"));
+        Console.WriteLine(Simplify("8/4"));
+    }
+
+    public static string Simplify(string fraction) {
+        var parts = fraction.Split("/");
+        var numerator = int.Parse(parts[0]);
+        var denominator = int.Parse(parts[1]);
+
+        //  If it can be represented as an integer, do the division and return it
+        if (numerator % denominator == 0) {
+            return $"{numerator/denominator}";
+        }
+
+        var biggestDenominator = GetBiggestCommonDenominator(numerator, denominator);
+
+        return $"{numerator / biggestDenominator}/{denominator / biggestDenominator}";
+    }
+
+    /// <summary>
+    /// Gets the biggest denominator for two numbers
+    /// </summary>
+    public static int GetBiggestCommonDenominator(int val1, int val2) {
+        // Get the bigger number
+        var max = Math.Max(val1, val2);
+
+        // Calculate the biggest denominator which can divide both the numbers
+        var biggestDenominator = Enumerable.Range(1, max).Where(i => val1 % i == 0 && val2 % i == 0).Last();
+
+        return biggestDenominator;
+    }
+}
---
+++ day4-1/readme.md
@@ -0,0 +1,14 @@
+## Day 4-1: Simplified Fractions *(very hard)*
+Create a function that returns the simplified version of a fraction.
+
+### Examples
+* `Simplify("4/6")` -> `"2/3"`
+* `Simplify("10/11")` -> `"10/11"`
+* `Simplify("100/400")` -> `"1/4"`
+* `Simplify("8/4")` -> `"2"`
+
+### Notes
+* A fraction is simplified if there are no common factors (except 1) between the numerator and the denominator. For example, `4/6` is not simplified, since 4 and 6 both share `2` as a factor.
+* If improper fractions can be transformed into integers, do so in your code (see example #4).
+#
+Done [X]
