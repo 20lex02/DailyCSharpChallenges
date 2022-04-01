a64011d12b0f60e5c2e83140b1df7869e62459df
Alexandre Laroche <LARA69060208@collegealma.ca>
2022-04-01 14:50:40 -0400

    Did day8

---
+++ day8/Program.cs
@@ -0,0 +1,83 @@
+﻿public static class Program {
+    public static void Main(string[] args) {
+        var tree = new BinaryTree<bool>(
+            new BinaryTree<bool>.Node(false,
+                new BinaryTree<bool>.Node(true),
+                new BinaryTree<bool>.Node(false,
+                    new BinaryTree<bool>.Node(true,
+                        new BinaryTree<bool>.Node(true),
+                        new BinaryTree<bool>.Node(true)
+                    ),
+                    new BinaryTree<bool>.Node(false)
+                )
+            )
+        );
+
+        Console.WriteLine(tree.GetUnivalTreeCount());
+    } 
+
+    public class BinaryTree<T> {
+        public Node Root;
+
+        public BinaryTree(Node root) {
+            Root = root;
+        }
+
+        public int GetUnivalTreeCount() {
+            var nodes = Root.GetAllDescendants().Where(node => node is not null);
+
+            return nodes.Where(node => node.GetAllDescendants().GoodDistinct((first, second) => Node.ValueEquals(first, second)).Count() <= 1).Count();
+        }
+
+        public class Node {
+            public Node? Left;
+            public Node? Right;
+            public T Value;
+
+            public Node(T value, Node? left = null, Node? right = null) {
+                Value = value;
+                Left = left;
+                Right = right;
+            }
+
+            public IEnumerable<Node> GetAllDescendants() {
+                Node current;
+                var uncheckedNodes = new Queue<Node>();
+                uncheckedNodes.Enqueue(this);
+
+                while (uncheckedNodes.TryDequeue(out current!)) {
+                    if (current.Left is not null) {
+                        yield return current.Left;
+                        uncheckedNodes.Enqueue(current.Left);
+                    }
+                    if (current.Right is not null) {
+                        yield return current.Right;
+                        uncheckedNodes.Enqueue(current.Right);
+                    }
+                } 
+            }
+
+            public static bool ValueEquals(Node? first, Node? second) {
+                if (first is null && second is null) {
+                    return true;
+                }
+
+                if (first is null || second is null) {
+                    return false;
+                }
+
+                return object.Equals(first.Value, second.Value);
+            }
+        }
+    }
+
+    public static IEnumerable<T> GoodDistinct<T>(this IEnumerable<T> source, Func<T, T, bool> comparator) {
+        List<T> distincts = new List<T>();
+        foreach (var elem in source) {
+            if (distincts.All(distinct => !comparator(distinct, elem))) {
+                distincts.Add(elem);
+                yield return elem;
+            }
+        }
+    }
+}
---
+++ day8/readme.md
@@ -0,0 +1,16 @@
+## Day 8: Unival trees (easy)
+A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.  
+Count the number of unival subtrees.
+### Example
+The following tree has 5 unival subtrees:
+```
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 1   1
+```
+#
+Done [X]
