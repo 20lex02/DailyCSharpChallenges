2834f4a6dab4d265739b5f7f3b0ee3784502a836
Alexandre Laroche <LARA69060208@collegealma.ca>
2022-04-06 12:11:40 -0400

    Did day13

---
+++ day13/Program.cs
@@ -0,0 +1,19 @@
+﻿public static class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine(LongestSubstring(2, "abcba"));
+    }
+    public static string LongestSubstring(int k, string s) {
+        var maximum = new List<char>();
+        for (int i = 0; i < s.Length - 1; i++) {
+            var currentLetters = new HashSet<char>();
+            var current = s.Skip(i).TakeWhile(character => {
+                currentLetters.Add(character);
+                return currentLetters.Count() <= 2;
+            }).ToList();
+
+            maximum = current.Count() > maximum.Count() ? current : maximum;
+        }
+
+        return string.Concat(maximum);
+    }
+}
---
+++ day13/readme.md
@@ -0,0 +1,6 @@
+## Day 13: Distinct Size *(hard)*
+Given an integer `k` and a string `s`, find the length of the longest substring that contains at most `k` distinct characters.
+### Examples
+ * `LongestSubstring(2, "abcba")` -> `"bcb"`
+#
+Done [X]
