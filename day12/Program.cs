4750c9a7b777d1099f0f6da983d6a0cf65a433ff
Alexandre Laroche <LARA69060208@collegealma.ca>
2022-04-06 11:48:20 -0400

    Did day12

---
+++ day12/Program.cs
@@ -0,0 +1,76 @@
+﻿public static class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine(StairsPossibilities(4, new[] {1, 2})); // 5
+    }
+
+    public static double StairsPossibilities(int numberOfSteps, int[] numberOfStepsAvailable) {
+        var maxSize = numberOfSteps / numberOfStepsAvailable.Min();
+        var minSize = (int)Math.Ceiling(numberOfSteps / (float)numberOfStepsAvailable.Max());
+        var orderedSteps = numberOfStepsAvailable.Distinct().OrderByDescending(n => n).ToList();
+
+        var numberOfPossibilities = 0.0;
+
+        foreach (var possibility in GetPossibilities(numberOfSteps, orderedSteps)) {
+            numberOfPossibilities += CaculateNumberOfPossibleArrengements(possibility.Length, possibility.Distinct().Count());
+        }
+        return numberOfPossibilities;
+    }
+
+    public static IEnumerable<int[]> GetPossibilities(int goal, List<int> possibleNumbers) {
+        if (possibleNumbers.Any()) {
+            // Get the first possible number and remove it from the possibilities
+            var startingNumber = possibleNumbers.First();
+            possibleNumbers.RemoveAt(0);
+
+            var currentGoalValue = goal;
+
+            if (possibleNumbers.Any()) {
+                // If there is any possible number left...
+                for (int i = startingNumber; i < goal; i += startingNumber) {
+                    // Decrease the current goal by the starting number
+                    currentGoalValue -= startingNumber;
+
+                    //Get all direct possibilities
+                    var validPossibilities = possibleNumbers.Where(possibility => currentGoalValue % possibility == 0);
+
+                    foreach (var possibleNumber in possibleNumbers) {
+                        var rest = currentGoalValue - possibleNumber;
+                        // If the rest of the number after removing a possibile number is greater then 0...
+                        if (rest > 0) {
+                            // Get all its possible values....                (ToList() to make a copy of the list)
+                            var restPossibleValues = GetPossibilities(rest, possibleNumbers.ToList());
+                            foreach (var possibility in restPossibleValues) {
+                                // And return them, preceded by the corresponding number of the starting number and the current possible number
+                                yield return Enumerable.Repeat(startingNumber, i / startingNumber).Append(possibleNumber).Concat(possibility).ToArray();
+                            }
+                        }
+                    }
+                }
+            }
+            
+
+            if (goal % startingNumber == 0) {
+                // If the goal is directly dividable by the starting number return an array containing it the apropriate number of time
+                yield return Enumerable.Repeat(startingNumber, goal / startingNumber).ToArray();
+            }
+
+            // Get the possibilities for the next possible numbers and return them
+            foreach (var possibility in GetPossibilities(goal, possibleNumbers)) {
+                yield return possibility;
+            }
+        }
+    }
+
+    public static double CaculateNumberOfPossibleArrengements(int arrayLength, int numberOfDifferentValues) {
+        
+        return (Factorial(arrayLength)/Factorial(arrayLength - (numberOfDifferentValues - 1)));
+    }
+
+    public static double Factorial(int x) {
+        var value = 1.0;
+        for (ulong i = 1; i <= (ulong)x; i++) {
+            value *= i;
+        }
+        return value;
+    }
+}
---
+++ day12/readme.md
@@ -0,0 +1,13 @@
+## Day 12: Staircase Troubles *(hard)*
+There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+### Example
+* `StairsPossibilities(4)` -> `5`  
+In this case, there are five unique ways to climb the staircase:
+  * `[1,1,1,1]`
+  * `[2,1,1]`
+  * `[1,2,1]`
+  * `[1,1,2]`
+  * `[2,2]`
+#
+Done [X]  
+Bonus: What if instead of being able to climb 1 or 2 steps at the time, you voulf climb any number from a set of positive integers X? For example, if X = `[1,3,5]`, you could climb 1, 3 or 5 steps at a time. [X]
