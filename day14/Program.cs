82e43269f524d9b0c9da85273047e02814ae9906
Alexandre Laroche <LARA69060208@collegealma.ca>
2022-04-08 10:30:37 -0400

    Did day14

---
+++ day14/Program.cs
@@ -0,0 +1,29 @@
+﻿public static class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine(EstimatePi(3));
+        Console.WriteLine(EstimatePi(5));
+    }
+
+    public static double EstimatePi(int digits) {
+        var random = new Random();
+        var tolerance = 1 / (Math.Pow(10, digits));
+        var approximation = 0d;
+
+        double x, y;
+        int numberOfCorrectGuesses = 0, totalGuesses = 0;
+
+        while (Math.Abs( Math.PI - approximation ) > tolerance) {
+            x = random.NextDouble() * 2 - 1;
+            y = random.NextDouble() * 2 - 1;
+
+            if (x * x + y * y <= 1) {
+                numberOfCorrectGuesses++;
+            }
+            totalGuesses ++;
+
+            approximation = 4 * ((double)numberOfCorrectGuesses / (double)totalGuesses);
+        }
+
+        return Math.Round(approximation, digits, MidpointRounding.AwayFromZero);
+    }
+}
---
+++ day14/readme.md
@@ -0,0 +1,4 @@
+## Day 14: Estimating Pi *(medium)*
+The area of a circle is defined as πr^2. Create a function that estimates π to `n` decimal places using a Monte Carlo method.
+#
+Done [X]
