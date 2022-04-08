79f130b567921023990e560b2b599eba4e024939
Alexandre Laroche <LARA69060208@collegealma.ca>
2022-04-08 14:06:38 -0400

    Did day15

---
+++ day15/Program.cs
@@ -0,0 +1,29 @@
+﻿public static class Program {
+    public static void Main(string[] args) {
+        var dataSource = Enumerable.Range(0, 100);
+        var encounteredValues = new HashSet<int>();
+        while (encounteredValues.Count() != dataSource.Count()) {
+            var randomValue = dataSource.GetRandom();
+            encounteredValues.Add(randomValue);
+            Console.WriteLine(randomValue);
+        } 
+    }
+
+    public static T GetRandom<T>(this IEnumerable<T> source, Random? random = null) {
+        var _random = random ?? new Random();
+
+        var count = 0;
+        var current = default(T);
+        foreach (var item in source) {
+            count++;
+            if (_random.Next(count) == 0) {
+                current = item;
+            }
+        }
+        if (count == 0) {
+            throw new InvalidOperationException();
+        }
+
+        return current!;
+    }
+}
---
+++ day15/readme.md
@@ -0,0 +1,6 @@
+## Day 15: Random Picker *(medium)*
+Given an Enumerable too large to store in memory, pick a random element from the collection with uniform probability.
+#
+Done [X]  
+Bonus: What if you can't access the size of the Enumerable? [X]  
+Bonus 2: Can you do it in one pass? [X]
